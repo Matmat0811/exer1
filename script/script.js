c4cd35d043be4866546d7e6646af2741f9c8e6a5
Matmat0811 <143145031+Matmat0811@users.noreply.github.com>
2023-09-08 10:09:26 +0800

    Update script.js
    add subtract funtion

--- script/script.js
+++ script/script.js
@@ -7,3 +7,13 @@ function displaySum() {
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+
+function displayMinus() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('minusButton').addEventListener("click", displayMinus);
